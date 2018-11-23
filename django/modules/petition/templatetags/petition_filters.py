0bf040244f971952711dd96088dc68444325d13f
meks <comms@meks.io>
2018-11-23 14:56:41 +0100

    different whatsapp link on desktop user agent

--- django/modules/petition/templatetags/petition_filters.py
+++ django/modules/petition/templatetags/petition_filters.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 from django.forms.fields import CheckboxInput
 
@@ -11,3 +12,13 @@ def field_type(field, ftype):
     except:
         pass
     return False
+
+pattern = re.compile(r'iphone|ipod|android|blackberry|mini|windows|palm')
+
+@register.filter(name='is_mobile')
+def is_mobile(request):
+    print(pattern.search(get_user_agent(request)))
+    return pattern.search(get_user_agent(request))
+
+def get_user_agent(request):
+    return request.META['HTTP_USER_AGENT'].lower()
