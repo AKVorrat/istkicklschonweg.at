b05d9df0075398e17224e174278823bf4b807b5f
Benedikt Gollatz <ben@differentialschokolade.org>
2019-04-16 16:19:07 +0200

    Activate sins admin

--- django/modules/petition/admin.py
+++ django/modules/petition/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Signature
+from .models import Signature, Sin
 
 class SignatureAdmin(admin.ModelAdmin):
     pass
 
+class SinAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Signature, SignatureAdmin)
+admin.site.register(Sin, SinAdmin)
